08ce33345ed64a032613db63b63a4d7fa5e77713
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-08-05 22:01:55 -0400

    adding lots of files for middleware, and authorization. sorry you cant pass. you need to see Mr. Frodo and Aragorn first

---
+++ auth/auth-router.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const Users = require('../users/users-model')
+
+router.post('/register', async (req, res) => {
+  const user = req.body;
+  const has = bcrypt.hasSync(user.password, 12);
+  user.password = hash;
+
+try{
+  const saved = await Users.add(user);
+  res.status(201).json(saved);
+} catch (err) {
+    console.log(err)
+    res.status(500).json(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  let {username, password} = req.body;
+
+  try {
+    const user = await Users.findBy({ username }).first()
+    if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user = user;
+      res.status(200).json({ message: 'The North Remembers'})
+    } else {
+      res.status(401).json({ message: 'You shall not pass!'});
+    }
+  } catch (err) {
+      res.status(500).json(error);
+  }
+});
+
+
+router.delete('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        res.status(400).json({ message: 'There was an error trying to leave The Shire:', error: err})
+      } else {
+        res.json({ message: 'Come back for Elevensies'});
+      }
+    });
+  } else {
+    res.end();
+  }
+})
+
+module.exports = router;
